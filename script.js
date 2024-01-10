b0fe140a0d93ec38bb182ee0c719ea8fd67c4ded
shantanusingh04 <forfrontenddev@gmail.com>
2024-01-10 23:05:29 +0530

    Clearing user Interface

--- script.js
+++ script.js
@@ -1,50 +1,70 @@
-const itemForm=document.getElementById('item-form');
-const itemInput=document.getElementById('item-input');
-const itemList=document.getElementById('item-list');
-const clearBtn=document.getElementById('clear')
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
-function addItem(e){
-e.preventDefault();
+function addItem(e) {
+  e.preventDefault();
 
-const newItem=itemInput.value;
-if(newItem===''){
-    alert('Please enter item');
+  const newItem = itemInput.value;
+  if (newItem === "") {
+    alert("Please enter item");
     return;
-}
-const li=document.createElement('li');
-li.appendChild(document.createTextNode(newItem))
-const button=createButton('remove-item btn-link text-red')
-li.appendChild(button)
-itemList.appendChild(li);
-itemInput.value==='';
-}
-
-function createButton(classes){
-    const button=document.createElement('button')
-    button.className=classes
-    const icon=createIcon('fa-solid fa-xmark')
-    button.appendChild(icon)
-    return button
+  }
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  itemList.appendChild(li);
+  checkUI();
+  itemInput.value === "";
 }
 
-function createIcon(iclasses){
-    const icon=document.createElement('i')
-    icon.className=iclasses
-    return icon
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
 }
 
-function removeItem(e){
-    if(e.target.parentElement.classList.contains('remove-item')){
-        e.target.parentElement.parentElement.remove()
+function createIcon(iclasses) {
+  const icon = document.createElement("i");
+  icon.className = iclasses;
+  return icon;
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure you want to delete")) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
     }
+  }
 }
 
-function clearItems(){
-    while(itemList.firstChild){
-        itemList.removeChild(itemList.firstChild)
-    }
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI()
 }
-itemForm.addEventListener('submit',addItem)
-itemList.addEventListener('click',removeItem)
-clearBtn.addEventListener('click',clearItems)
 
+function checkUI() {
+  const items = document.querySelectorAll("li");
+  console.log(items);
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+
+checkUI();
