549b47e7366b7849859d94f52c4edb1692a58bf1
shantanusingh04 <forfrontenddev@gmail.com>
2024-01-24 01:38:14 +0530

    Item add to locStore

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const itemList = document.getElementById("item-list");
 const clearBtn = document.getElementById("clear");
 const itemFilter = document.getElementById("filter");
 
-function addItem(e) {
+function onAddItemSubmit(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
@@ -12,13 +12,30 @@ function addItem(e) {
     alert("Please enter item");
     return;
   }
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem));
+  addItemToDOM(newItem)
+  addItemToStorage(newItem)
+  checkUI();
+  itemInput.value === "";
+}
+
+function addItemToDOM(item){
+    const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   itemList.appendChild(li);
-  checkUI();
-  itemInput.value === "";
+}
+
+function addItemToStorage(item){
+  let itemsFromStorage;
+  if(localStorage.getItem('items')===null){
+    itemsFromStorage=[];
+  }
+  else{
+    itemsFromStorage=JSON.parse(localStorage.getItem('items'))
+  }
+  itemsFromStorage.push(item);
+  localStorage.setItem('items',JSON.stringify(itemsFromStorage))
 }
 
 function createButton(classes) {
@@ -78,7 +95,7 @@ function checkUI() {
   }
 }
 
-itemForm.addEventListener("submit", addItem);
+itemForm.addEventListener("submit", onAddItemSubmit);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
 itemFilter.addEventListener("input", filterItems);
