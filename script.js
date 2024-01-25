03abf5c5c981d7551c52404184bb8f441448e35d
shantanusingh04 <forfrontenddev@gmail.com>
2024-01-25 23:59:10 +0530

    Remove feature in locStore

--- script.js
+++ script.js
@@ -64,19 +64,29 @@ function getItemsFromStorage(){
   return itemsFromStorage
 }
 
-function removeItem(e) {
-  if (e.target.parentElement.classList.contains("remove-item")) {
-    if (confirm("Are you sure you want to delete")) {
-      e.target.parentElement.parentElement.remove();
-      checkUI();
-    }
+function onClickItem(e){
+  if (e.target.parentElement.classList.contains("remove-item")){
+removeItem(e.target.parentElement.parentElement)
   }
+}
+function removeItem(item) {
+  if(confirm("Are you sure?")){
+    item.remove()
+    removeItemFromStorage(item.textContent)
+    checkUI()
+  }
+}
+function removeItemFromStorage(item){
+  let itemsFromStorage=getItemsFromStorage();
+  itemsFromStorage=itemsFromStorage.filter((i)=>i!==item);
+  localStorage.setItem('items',JSON.stringify(itemsFromStorage))
 }
 
 function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  localStorage.removeItem('items')
   checkUI()
 }
 
@@ -109,7 +119,7 @@ function checkUI() {
 
 function init(){
 itemForm.addEventListener("submit", onAddItemSubmit);
-itemList.addEventListener("click", removeItem);
+itemList.addEventListener("click", onClickItem);
 clearBtn.addEventListener("click", clearItems);
 itemFilter.addEventListener("input", filterItems);
 document.addEventListener('DOMContentLoaded',displayItems)
