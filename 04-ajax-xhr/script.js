5e8b82c99219fa88d7fdfa37d4ea5479c6ba418f
shantanusingh04 <forfrontenddev@gmail.com>
2024-02-08 19:48:18 +0530

    xml and ajax

---
+++ 04-ajax-xhr/script.js
@@ -0,0 +1,18 @@
+const xhr=new XMLHttpRequest();
+
+// xhr.open('GET','./movies.json');
+xhr.open('GET','https://api.github.com/users/bradtraversy/repos')
+
+xhr.onreadystatechange=function(){
+if(this.readyState===4&&this.status===200){
+    // console.log(JSON.parse(this.responseText))
+    const data=JSON.parse(this.responseText)
+    data.forEach((repo)=>{
+        const li=document.createElement('li')
+        li.innerHTML=`<strong>${repo.name}</strong> -
+         ${repo.description}`
+         document.querySelector('#results').appendChild(li)
+    })
+}
+}
+xhr.send();
